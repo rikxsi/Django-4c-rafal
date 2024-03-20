6f23d6531355d36665a81a433ae48dd3671cc52c
rafalbla <rafalbla.gmail.com>
2024-03-20 08:58:15 +0100

    ada

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,7 +10,8 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
